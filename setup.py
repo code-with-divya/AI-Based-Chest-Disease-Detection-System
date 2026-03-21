db7d1efa180370e65951cd32d91a09096a5fc458
Divya Gawade <divyagawade@gmail.com>
2026-03-21 19:51:18 +0530

    smallchange

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ChestDiseaseClassifier",
+    name="AI-Based-Chest-Disease-Detection",
     version="0.0.1",
     author="DivyaGawade",
     author_email="gawadedivya15@gmail.com",
-    url=f"https://github.com/KalyanM45/End-to-End-Chest-Disease-Classification",
+    url=f"https://github.com/code-with-divya/AI-Based-Chest-Disease-Detection-System",
     packages=find_packages(),   
 )
